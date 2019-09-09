9f87455f862b9d59e45fe902013dac7114342db6
Eddie <eddie@diggle.tech>
2019-09-09 13:56:05 +0200

    This were done here

--- lib/auth/login/login_form.dart
+++ lib/auth/login/login_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provisional_license_test/auth/login/login.dart';
+import 'package:provisional_license_test/auth/login/signin_button.dart';
 
 class LoginForm extends StatefulWidget {
   @override
@@ -37,34 +38,46 @@ class _LoginFormState extends State<LoginForm> {
       child: BlocBuilder<LoginEvent, LoginState>(
         bloc: _loginBloc,
         builder: (
-            BuildContext context,
-            LoginState state,
-            ) {
-          return Form(
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'username'),
-                  controller: _usernameController,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'password'),
-                  controller: _passwordController,
-                  obscureText: true,
-                ),
-                RaisedButton(
-                  onPressed:
-                  state is! LoginLoading ? _onLoginButtonPressed : null,
-                  child: Text('Login'),
-                ),
-                Container(
-                  child: state is LoginLoading
-                      ? CircularProgressIndicator()
-                      : null,
-                ),
-              ],
-            ),
-          );
+          BuildContext context,
+          LoginState state,
+        ) {
+          return Container(
+              margin: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(25.0))),
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Form(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "What's your email address?",
+                          labelText: "Email,"),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: "Password",
+                        hintText: "Try 'bears'...",
+                      ),
+                      obscureText: true,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SigninButton(
+                          child: Text("Sign In",
+                              style: TextStyle(
+                                  fontFamily: "RobotoMedium",
+                                  fontSize: 16,
+                                  color: Colors.white)),
+                          onPressed: () {}),
+                    )
+                  ],
+                )),
+              ));
         },
       ),
     );
--- lib/auth/login/login_page.dart
+++ lib/auth/login/login_page.dart
@@ -6,6 +6,16 @@ import 'package:provisional_license_test/auth/user_repository.dart';
 import 'package:provisional_license_test/auth/authentication.dart';
 import 'package:provisional_license_test/auth/login/login.dart';
 
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:provisional_license_test/auth/authentication.dart';
+import 'package:provisional_license_test/auth/authentication_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 class LoginPage extends StatelessWidget {
   final UserRepository userRepository;
 
@@ -15,18 +25,104 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: BlocProvider(
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    return Material(
+      child: BlocProvider(
         builder: (context) {
           return LoginBloc(
             authenticationBloc: BlocProvider.of<AuthenticationBloc>(context),
             userRepository: userRepository,
           );
         },
-        child: LoginForm(),
+        child: Container(
+          color: Theme.of(context).primaryColorDark,
+          child: Stack(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  FractionallySizedBox(
+                    heightFactor: 1,
+                    child: ClipPath(
+                      clipper: WaveClipperTwo(),
+                      child: Container(
+                        color: Theme.of(context).primaryColorLight,
+                      ),
+                    ),
+                  ),
+                  FractionallySizedBox(
+                    heightFactor: 3 / 5,
+                    child: ClipPath(
+                      clipper: DiagonalPathClipperOne(),
+                      child: Container(
+                        color: Theme.of(context).accentColor,
+                      ),
+                    ),
+                  ),
+                  FractionallySizedBox(
+                    heightFactor: 2 / 5,
+                    child: ClipPath(
+                      clipper: WaveClipperTwo(),
+                      child: Container(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SafeArea(
+                bottom: false,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(
+                        left: 16.0,
+                        right: 16.0,
+                        top: 64.0,
+                        bottom: 32.0,
+                      ),
+                      child: Text(
+                        "Learn to drive",
+                        style: Theme.of(context).textTheme.display3.copyWith(
+                              color: Colors.white,
+                            ),
+                      ),
+                    ),
+                    LoginForm(),
+                    Padding(
+                      padding: EdgeInsets.all(16),
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            side: BorderSide(
+                              color: Theme.of(context).brightness ==
+                                      Brightness.light
+                                  ? Theme.of(context).primaryColor
+                                  : Colors.white,
+                            )),
+                        color: Theme.of(context).backgroundColor,
+                        onPressed: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Text(
+                            "Skip for now",
+                            style: Theme.of(context).textTheme.body1.copyWith(
+                                  color: Theme.of(context).brightness ==
+                                          Brightness.light
+                                      ? Theme.of(context).primaryColor
+                                      : Colors.white,
+                                ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
